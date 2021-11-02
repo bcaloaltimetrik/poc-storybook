aba027bcdd1c3514ef9facdf17525f19af5aa29f
Brahian Calo <brahian.calo@salesforce.com>
2021-11-02 16:28:25 -0300

    Add size api and complete implementation of poc-icon

--- src/modules/poc/icon/icon.ts
+++ src/modules/poc/icon/icon.ts
@@ -1,5 +1,16 @@
 import { LightningElement, api } from 'lwc'
+import cn from 'classnames'
 
+const BASE_URL = '/svgs/spritesheet.svg'
 export default class Icon extends LightningElement {
   @api size: string = 'medium'
+  @api iconName: string
+
+  get iconHref(): string {
+    return `${BASE_URL}#${this.iconName}`
+  }
+
+  get iconClass(): string {
+    return cn(this.size && `size--${this.size}`)
+  }
 }
--- stories/icon.stories.ts
+++ stories/icon.stories.ts
@@ -4,18 +4,29 @@ import { buildAndRegisterCustomElement } from './utils'
 
 buildAndRegisterCustomElement('poc-icon', Icon)
 
+const sizeOptions = ['medium', 'small', 'large']
+const iconOptions = ['building', 'location']
+
 export default {
   title: 'Example/Icon',
   argTypes: {
     size: {
       control: {
         type: 'select',
-        options: ['small', 'medium', 'large'],
+        options: sizeOptions,
+      },
+    },
+    iconName: {
+      control: {
+        type: 'select',
+        options: iconOptions,
       },
     },
   },
 }
 
-const Template = ({ size }) => html` <poc-icon></poc-icon> `
+const Template = ({ size = sizeOptions[0], iconName = iconOptions[0] }) => html`
+  <poc-icon size="${size}" icon-name="${iconName}"></poc-icon>
+`
 
 export const Base = Template.bind({})
